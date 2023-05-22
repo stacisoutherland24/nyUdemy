dde0beb0f2f5553a388f137dbb4ba9eea9922136
Staci Southerland <stacisoutherland24@gmail.com>
2023-05-22 09:45:47 -0500

    Assignment 2

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template: ` <app-server> </app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
-export class ServersComponent {}
+export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+  username = '';
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
+  ngOnInIt() {}
+
+  onCreateServer() {
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onUpdateUserName(event: Event) {
+    this.username = (<HTMLInputElement>event.target).value;
+  }
+}
